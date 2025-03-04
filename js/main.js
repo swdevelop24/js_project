58fb2a98e31bae96b945b499d3fb60617e12ad3c
swdevelop24 <swdevelop24@gmail.com>
2025-03-04 10:09:38 -0600

    test: test secretes on github

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-import { clientId, clientSecret } from "./keys.js";
+import { clientId } from "./keys.js";
+// import { clientSecret } from "./keys.js";
+
 
 console.log(`clientId: ${clientId}`);
-console.log(`clientSecret: ${clientSecret}`);
+console.log(`clientSecret: ${CLIENTSECRET}`);
+
