0f0004c41484c6a2f5509d25e3013168c7e7a16e
swdevelop24 <swdevelop24@gmail.com>
2025-03-04 15:01:38 -0600

    test: api

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-import { clientId } from "./keys.js";
-// import { clientSecret } from "./keys.js";
+// import { clientId } from "./keys.js";
+// // import { clientSecret } from "./keys.js";
 
 
-console.log(`clientId: ${clientId}`);
-console.log(`clientSecret: ${CLIENTSECRET}`);
+// console.log(`clientId: ${clientId}`);
+// console.log(`clientSecret: ${CLIENTSECRET}`);
+
+// const apiKey = process.env.API_KEY;
+const apiKey = process.env.API_KEY;
+
+fetch(`https://developer.nps.gov/api/v1/parks?limit=1&api_key=${apiKey}`)
+  .then(response => response.json())
+  .then(data => console.log(data.data[0].fullName))
+  .catch(error => console.error('Error:', error));
 
