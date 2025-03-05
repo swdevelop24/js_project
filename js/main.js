3d0797310b3d2199943d2781d2ca7fc453824648
swdevelop24 <swdevelop24@gmail.com>
2025-03-04 20:55:04 -0600

    feat: add cdn settings

--- js/main.js
+++ js/main.js
@@ -1,13 +1,1 @@
-// import { clientId } from "./keys.js";
-// // import { clientSecret } from "./keys.js";
-
-
-// console.log(`clientId: ${clientId}`);
-// console.log(`clientSecret: ${CLIENTSECRET}`);
-
-// const apiKey = process.env.API_KEY;
-const apiKey = 'API_KEY';
-fetch(`https://developer.nps.gov/api/v1/parks?limit=1&api_key=${apiKey}`)
-  .then(response => response.json())
-  .then(data => console.log(data.data[0].fullName))
-  .catch(error => console.error('Error:', error));
+let api_key="";
