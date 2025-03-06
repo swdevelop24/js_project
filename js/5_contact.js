4b9330205a06cc82eb8014f5453700947c5c1ff7
HJ Kim <lalahabi12@gmail.com>
2025-03-07 05:04:26 +0900

    fix-nav바 햄버거 버튼 안보이는거 수정

--- js/5_contact.js
+++ js/5_contact.js
@@ -16,3 +16,11 @@ function toggleMenu() {
 
 getParkInfo()
 */
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+}
